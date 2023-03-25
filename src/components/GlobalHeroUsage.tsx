13243167b66b80feb27eb10b76c8dc32fe7fb440
Ryan Luong <ryanluong83@gmail.com>
2023-03-24 21:40:09 -0700

    Got team picker component done

--- src/components/GlobalHeroUsage.tsx
+++ src/components/GlobalHeroUsage.tsx
@@ -2,8 +2,9 @@ import React, {useState, useEffect} from 'react'
 import MapTypePicker from "./MapTypePicker.tsx"
 import { MapType } from './MapType';
 import { Team } from './Team'
+import TeamPicker from './TeamPicker.tsx';
 
-const listOfMapType: MapType[] = [
+const listOfMapTypes: MapType[] = [
     {typeName: "assult", checkedState: true},
     {typeName: "control", checkedState: true},
     {typeName: "hybrid", checkedState: true},
@@ -11,21 +12,55 @@ const listOfMapType: MapType[] = [
     {typeName: "push", checkedState: true},
   ]
 
-const listOfTeam: Team[] = [
+const listOfTeams: Team[] = [
     {teamName: "Atlanta Reign", checkedState: true},
+    {teamName: "Boston Uprising", checkedState: true},
+    {teamName: "Chengdu Hunter", checkedState: true},
+    {teamName: "Dallas Fuel", checkedState: true},
+    {teamName: "Florida Mayhem", checkedState: true},
+    {teamName: "Guangzhou Charge", checkedState: true},
+    {teamName: "Hangzhou Spark", checkedState: true},
+    {teamName: "Houston Outlaws", checkedState: true},
+    {teamName: "London Spitfire", checkedState: true},
+    {teamName: "Los Angeles Gladiators", checkedState: true},
+    {teamName: "Los Angeles Valiant", checkedState: true},
+    {teamName: "New York Excelsior", checkedState: true},
+    {teamName: "San Francisco Shock", checkedState: true},
+    {teamName: "Seoul Dynasty", checkedState: true},
+    {teamName: "Seoul Infernal", checkedState: true},
+    {teamName: "Shanghai Dragons", checkedState: true},
+    {teamName: "Toronto Defiant", checkedState: true},
+    {teamName: "Vancouver Titans", checkedState: true},
+    {teamName: "Vegas Eternal", checkedState: true},
+    {teamName: "Washington Justice", checkedState: true},
 ]
 
+const arrayOfTeamName: string[] = listOfTeams.map((team) =>
+    team.teamName
+)
+
 const GlobalHeroUsage = () => {
-    const [arrayOfMapType, updateArrayOfMapType] = useState<MapType[]>(listOfMapType)
+    const [arrayOfMapType, updateArrayOfMapType] = useState<MapType[]>(listOfMapTypes)
+    const [arrayOfTeams, updateArrayOfTeams] = useState<Team[]>(listOfTeams)
+    
     const updateArrayOfMapTypeHelper = (newArray: string[]): void => {
-        updateArrayOfMapType(prevState => prevState.map(mapType => ({
+        updateArrayOfMapType(prevState => prevState.map((mapType) => ({
             ...mapType,
             checkedState: newArray.includes(mapType.typeName) ? true : false
         })))
-        console.log(arrayOfMapType)
     }
+
+    const updateArrayOfTeamsHelper = (newArray: string[]): void => {
+        updateArrayOfTeams(prevState => prevState.map((team) => ({
+            ...team,
+            checkedState: newArray.includes(team.teamName) ? true : false
+        })))
+    }
+
+    console.log(arrayOfTeamName)
     return(
         <div>
+            <TeamPicker listOfTeams={arrayOfTeamName} parentFunction={updateArrayOfTeamsHelper}/>
             <MapTypePicker parentFunction={updateArrayOfMapTypeHelper}/>
         </div>
     )
--- src/components/TeamPicker.tsx
+++ src/components/TeamPicker.tsx
@@ -1,35 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import {TextField, Autocomplete} from '@mui/material'
+function TeamPicker(props: {listOfTeams: string[], parentFunction: Function}) {
+    const [teams, updateTeams] = useState<string[]>(props.listOfTeams)
 
-function TeamPicker() {
-    const [teamName, setTeamName] = useState("Atlanta Reign");
+    const updateTeamsHelper = (newArray) => {
+        updateTeams([...newArray])
+    }
+
+    const handleChange = (newArray) => {
+        updateTeamsHelper(newArray)
+        props.parentFunction(newArray)
+    }
     
     return(
         <div>
             <p>
                 Team
             </p>
-            <select name="team">
-                <option value="Atlanta Reign"> Atlanta Reign </option>
-                <option value="Boston Uprising"> Boston Uprising </option>
-                <option value="Chengdu Hunter"> Chengdu Hunters </option>
-                <option value="Dallas Fuel"> Dallas Fuel </option>
-                <option value="Florida Mayhem"> Florida Mayhem </option>
-                <option value="Guangzhou Charge"> Guangzhou Charge </option>
-                <option value="Hangzhou Spark"> Hangzhou Spark </option>
-                <option value="Houston Outlaws"> Houston Outlaws </option>
-                <option value="London Spitfire"> London Spitfire </option>
-                <option value="Los Angeles Gladiators"> Los Angeles Gladiators </option>
-                <option value="Los Angeles Valiant"> Los Angeles Valiants </option>
-                <option value="New York Excelsior"> New York Excelsior </option>
-                <option value="San Francisco Shock"> San Francisco Shock </option>
-                <option value="Seoul Dynasty"> Seoul Dynasty </option>
-                <option value="Seoul Infernal"> Seoul Infernal </option>
-                <option value="Shanghai Dragons"> Shanghai Dragons </option>
-                <option value="Toronto Defiant"> Toronto Defiant </option>
-                <option value="Vancouver Titans"> Vancouver Titans </option>
-                <option value="Vegas Eternal"> Vegas Eternal </option>
-                <option value="Washington Justice"> Washington Justice </option>
-            </select>
+            <div>
+            <Autocomplete
+            multiple
+            id="checkboxes-tags-demo"
+            options={[...props.listOfTeams]}
+            disableCloseOnSelect
+            getOptionLabel={(teamName) => teamName}
+            isOptionEqualToValue={(option, value) => option === value}
+            onChange={(event, newArray) => handleChange(newArray)}
+            defaultValue={[...props.listOfTeams]}
+            style={{ width: 500 }}
+            renderInput={(params) => (
+                <TextField {...params} label={teams.length === 5 ? "All" : "Multiple Values"}/>
+            )}
+            />
+            </div>
         </div>
     )
 }
