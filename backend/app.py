b75ab40c2bbda777224dcd35410ecaa767f1db41
Ryan Luong <ryanluong83@gmail.com>
2023-03-26 22:41:37 -0700

    Starting the RESTful API

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,26 @@
 from flask import Flask
-from aws_helper_functions.helper_functions import get_bucket
+from aws_helper_functions.helper_functions import get_s3
+import boto3
+import io
+import pandas as pd
+import csv
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/overwatch_league/2022/map_types/')
-def list_bucket():
-    client = get_bucket()
-    client_response = client.list_buckets()
-    buckets = []
-    for bucket in client_response['Buckets']:
-        print(f'Bucket name {bucket["Name"]}')
-        buckets.append(bucket["Name"])
-    return buckets
+@app.route('/overwatch-league/2022/<stage>/map-pools', methods=["GET"])
+def get_all_map_types(stage):
+    s3 = get_s3()
+    map_pool_file = f"main/{stage}-map-pools.csv"
+    map_pool = s3.Object(bucket_name="overwatch-league-bucket", key=map_pool_file)
+    print(map_pool.key)
+    response = map_pool.get()
+    lines = response['Body'].read().split()
+    for row in csv.DictReader(lines):
+        print(row)
+    return "HI"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/aws_helper_functions/helper_functions.py
+++ backend/aws_helper_functions/helper_functions.py
@@ -4,13 +4,14 @@ import os
 
 load_dotenv()
 
-def get_bucket():
+def get_s3():
     aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
     aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-    client = boto3.client(
+    s3 = boto3.resource(
         's3',
         aws_access_key_id = aws_access_key_id,
         aws_secret_access_key = aws_secret_access_key,
         endpoint_url='http://localhost:4566'
     )
-    return client
+    # bucket = s3.Bucket('overwatch-league-bucket')
+    return s3
