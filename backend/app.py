5f7b1a30d0dc06fa9f283f31231142b80b2ea08d
Ryan Luong <ryanluong83@gmail.com>
2023-03-27 19:57:35 -0700

    Basic API call done

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,56 @@
-from flask import Flask
+from flask import Flask, make_response, request, jsonify
 from aws_helper_functions.helper_functions import get_s3
 import boto3
 import io
 import pandas as pd
 import csv
+from io import StringIO
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/overwatch-league/2022/<stage>/map-pools', methods=["GET"])
-def get_all_map_types(stage):
-    s3 = get_s3()
-    map_pool_file = f"main/{stage}-map-pools.csv"
-    map_pool = s3.Object(bucket_name="overwatch-league-bucket", key=map_pool_file)
-    print(map_pool.key)
-    response = map_pool.get()
-    lines = response['Body'].read().split()
-    for row in csv.DictReader(lines):
-        print(row)
-    return "HI"
+@app.route('/overwatch-league/2022/<string:stage>/map-pools', methods=["GET"])
+def get_map_pools_from_stage(stage):
+    if (request.method == "GET"):
+        s3 = get_s3()
+        map_pools_file_name = f"main/{stage}-map-pools.csv"
+        map_pools_file = s3.Object(bucket_name="overwatch-league-bucket", key=map_pools_file_name)
+        response = map_pools_file.get()
+        map_pool_csv_format = response['Body'].read().decode("utf-8")
+        buffer = StringIO(map_pool_csv_format)
+        reader = csv.DictReader(buffer)
+        all_maps = list(reader)
+        # print(map_pool)
+        map_pools = []
+        for map in all_maps:
+            map_pool = {"map_name": map["map_name"], "map_type": map["map_type"]}
+            map_pools.append(map_pool)
+            
+        return map_pools, 200
+
+@app.route('/overwatch-league/2022/<string:stage>/heroes-usage', methods=["GET"])
+def get_hero_usage_from_stage(stage):
+    if (request.method == "GET"):
+        s3 = get_s3()
+        heroes_usage_file_name = f"heroes-usage/{stage}-global-heroes-usage.csv"
+        heroes_usage_file = s3.Object(bucket_name="overwatch-league-bucket", key=heroes_usage_file_name)
+        response = heroes_usage_file.get()
+        hero_usage_csv_format = response['Body'].read().decode("utf-8")
+        buffer = StringIO(hero_usage_csv_format)
+        reader = csv.DictReader(buffer)
+        heroes_usage = list(reader)
+        return heroes_usage, 200
+
+
+
+    # map_pool_df = pd.read_csv(StringIO(lines["Body"].read().decode('utf-8')))
+    # for row in csv.DictReader(lines):
+    #     print(row)
+    # print(map_pool_df)
+    # for line in lines:
+    #     print(line)
 
 if __name__ == '__main__':
     app.run(debug=True)
